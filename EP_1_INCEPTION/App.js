ff9c51a9a6326424de80564b227794b7d49a5b1a
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-03-10 09:46:08 +0530

    Day 1 - Hello World using  React CDN

---
+++ EP_1_INCEPTION/App.js
@@ -0,0 +1,15 @@
+const parent = React.createElement(
+    "div", 
+    {id: "parent"}, 
+    [
+        React.createElement("div", {id: "child1"}, 
+            React.createElement("h1", {}, "This is my heading1"),
+            React.createElement("h2", {}, "This is my heading2")
+        ),
+        React.createElement("div", {id: "child2"}, 
+            React.createElement("h1", {}, "This is my heading1"),
+            React.createElement("h2", {}, "This is my heading2")
+        )
+    ]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
