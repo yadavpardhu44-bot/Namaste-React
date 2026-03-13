198dc0aad8f34f65c47177352f16ec7ff3ae2416
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-03-13 21:36:23 +0530

    Episode 2 setup with parcel and notes

--- EP_1_INCEPTION/App.js
+++ EP_1_INCEPTION/App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 const parent = React.createElement(
     "div", 
     {id: "parent"}, 
     [
         React.createElement("div", {id: "child1"}, 
-            React.createElement("h1", {}, "This is my heading1"),
+            React.createElement("h1", {}, "Pardhu Yadav"),
             React.createElement("h2", {}, "This is my heading2")
         ),
         React.createElement("div", {id: "child2"}, 
