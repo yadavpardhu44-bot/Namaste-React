bfc92bf048386b5360d21fd0532bdc256781fe46
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-03-14 22:48:22 +0530

    JSX intro

--- EP_1_INCEPTION/App.js
+++ EP_1_INCEPTION/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, 
-    [
-        React.createElement("div", {id: "child1"}, 
-            React.createElement("h1", {}, "Pardhu Yadav"),
-            React.createElement("h2", {}, "This is my heading2")
-        ),
-        React.createElement("div", {id: "child2"}, 
-            React.createElement("h1", {}, "This is my heading1"),
-            React.createElement("h2", {}, "This is my heading2")
-        )
-    ]);
+
+alert("React is running");
+const jsxheading = (
+<h1 id="heading" className="jsxheading" tabIndex={5}>
+    Namaste React using JSX🚀
+</h1>
+);
+console.log(jsxheading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxheading);
