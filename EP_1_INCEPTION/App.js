5ae76eda328a4130c5fc6b1da288c758f6370242
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-03-15 10:27:24 +0530

    component in component

--- EP_1_INCEPTION/App.js
+++ EP_1_INCEPTION/App.js
@@ -2,24 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 alert("React is running");
-
+const Title = () => (
+<h1 id="heading" className="jsxheading" tabIndex={5}>
+    Namaste React using JSX🚀
+</h1>
+);
 
 const HeadingComponent = () =>{         //component composition
     return <div id="container">
-        {/* <Title></Title>
+        <Title></Title>
         <Title />
-        {Title()} */}
+        {Title()}
         <h1>
             Namaste React Using functional component
         </h1>
     </div>
 }
-
-const Title = (
-<h1 id="heading" className="jsxheading" tabIndex={5}>
-    <HeadingComponent></HeadingComponent>
-    Namaste React using JSX🚀
-</h1>
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(Title);
+root.render(<HeadingComponent />); 
