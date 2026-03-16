f7e1fad65663c57a3a820eb04358642ec2919cb0
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-03-16 21:21:30 +0530

    ep3 assignment part-2

--- EP_3_TALK_IS_CHEAP/APP.js
+++ EP_3_TALK_IS_CHEAP/APP.js
@@ -1,37 +1,33 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const react_element = React.createElement("div", {className: "title"},
-    [
-        React.createElement("h1",{key: "h1"},"This is H1 tag"),
-        React.createElement("h2",{key: "h2"},"This is H2 tag"),
-        React.createElement("h3",{key: "h3"},"This is H3 tag")
-    ]
-);
-
-//JSX
-const element = (
-    <div className="title">
-        <h1>This is H1 tag</h1>
-        <h2>This is H2 tag</h2>
-        <h3>This is H3 tag</h3>
-    </div>
-)
-
-const HeadingComponent = () =>{
-    return (
-        <h1>This is another heading</h1>
-    );
+const Logo = () => {
+    return(<img
+        className="logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="logo"
+    />)
 };
 
-const TitleComponent = () =>{
-    return (<div className="title" id="heading" tabIndex={5}>
-        <h1>This is H1 tag</h1>
-        <h2>This is H2 tag</h2>
-        <h3>This is H3 tag</h3>
-        <HeadingComponent></HeadingComponent>
+const SearchBar  = () => {
+    return(<input
+        type="text"
+        className="search"
+        placeholder="search..."
+    />)
+};
+
+const UserIcon = () =>{
+    return <div className="user">👤</div>
+};
+
+const Header = () =>{
+    return(<div className="header">
+        <Logo/>
+        <SearchBar/>
+        <UserIcon/>
     </div>)
 };
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitleComponent/>);
+root.render(<Header/>);
